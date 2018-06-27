e23a16a782e78ab3c05228816d9b9ee95ec1f38f
Mateus Braga <mateuscbraga@gmail.com>
2018-06-27 11:34:23 -0300

    First part officially done

--- src/navbar/navbar.jsx
+++ src/navbar/navbar.jsx
@@ -4,12 +4,12 @@ export default class Navbar extends Component {
     render(){
         return(
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={{'marginBottom': '1rem'}}>
-                <a className="navbar-brand" href="">TvTS</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <a className="navbar-brand" href="#">TvTS</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
                         <a className="nav-link" href="">Sinopses <span className="sr-only">(current)</span></a>
                     </li>
