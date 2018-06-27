03b5ec7e18a353fface612cc69863dfd3c6af04c
Mateus Braga <mateuscbraga@gmail.com>
2018-06-26 22:28:10 -0300

    initial commit

---
+++ src/navbar/navbar.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+    render(){
+        return(
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a class="navbar-brand" href="#">TvTS</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Shows</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Movies</a>
+                    </li>
+                    <li class="nav-item">
+                    </li>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+}
